0638799fbf784c7fb9c5f95d6c285d9739a3c1ba
Muskan Rana <ur6063690@gmail.com>
2025-09-27 19:51:34 +0530

    home page

---
+++ models/home.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../utils/pathUtil");
+
+module.exports = class Home {
+  constructor(houseName, price, location, Rating, PhotoURL) {
+    this.houseName = houseName;
+    this.price = price;
+    this.location = location;
+    this.Rating = Rating;
+    this.PhotoURL = PhotoURL;
+  }
+  save() {
+    Home.fetchAll((registeredHomes) => {
+      registeredHomes.push(this);
+      const homeDataPath = path.join(rootDir, "data", "homes.json");
+      fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), (error) => {
+        console.log("File writing concluded");
+      });
+    });
+  }
+
+  static fetchAll(callback) {
+    const homeDataPath = path.join(rootDir, "data", "homes.json");
+    fs.readFile(homeDataPath, (err, data) => {
+      console.log("file read", err, data);
+      if (!err) {
+        callback(JSON.parse(data));
+      } else {
+        callback([]);
+      }
+    });
+  }
+};
